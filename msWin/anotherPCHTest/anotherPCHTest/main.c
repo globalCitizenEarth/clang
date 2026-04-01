fb3ee0e0e291c6544d3afa4ca5161c7abc0b0382
WILLIAM BOSWALL <wboswall@gmail.com>
2026-04-01 08:36:19 -0300

    Commit new projects Clang
    made some test projects to see if .gch can be compiled properly after a certain check in codeblocks project files' properties especially the .h one to make sure compile is checked.

---
+++ msWin/anotherPCHTest/anotherPCHTest/main.c
@@ -0,0 +1,5 @@
+#include "pch.h" //Include precompiled header
+int main(){
+    printf("Testing precompiled header.");
+    return 0;
+}
---
+++ msWin/anotherPCHTest/anotherPCHTest/pch.h
@@ -0,0 +1,7 @@
+#ifndef PCH_H
+#define PCH_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define TEST_MACRO 100
+#endif // PCH_H
