e4f99542b5aa204881505c678d56a59de4a5928f
globalCitizenEarth <wboswall@gmail.com>
2026-04-17 20:12:42 -0300

    A Commit of old program files

---
+++ CLang101/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+<<<<<<< Updated upstream
+    printf("Hello world!\n");
+    printf("Press Enter to End Program\n");
+	system("Pause");
+=======
+    printf("Begin program\n\n");
+    printf("Hello world!\n\n");
+    printf("Hello C Language\n\n");
+    printf("Hello Dennis Ritchie\n\n");
+    printf("End program\n");
+>>>>>>> Stashed changes
+    return 0;
+}
